725554df4a24b9b63f031195cc71135af741f2d9
Hannah Garcia <hannah.kacso@gmail.com>
2024-11-12 03:50:22 +0000

    set up API

--- src/app.py
+++ src/app.py
@@ -8,7 +8,7 @@ from flask_swagger import swagger
 from flask_cors import CORS
 from utils import APIException, generate_sitemap
 from admin import setup_admin
-from models import db, User
+from models import db, User, People, Planet, Favorite
 #from models import Person
 
 app = Flask(__name__)
@@ -36,14 +36,75 @@ def handle_invalid_usage(error):
 def sitemap():
     return generate_sitemap(app)
 
-@app.route('/user', methods=['GET'])
-def handle_hello():
+# [GET] /people - Get a list of all the people
+@app.route('/people', methods=['GET'])
+def get_people():
+    people = People.query.all()
+    return jsonify([person.to_dict() for person in people])
 
-    response_body = {
-        "msg": "Hello, this is your GET /user response "
-    }
+# [GET] /people/<int:people_id> - Get one single person's information
+@app.route('/people/<int:people_id>', methods=['GET'])
+def get_person(people_id):
+    person = People.query.get_or_404(people_id)
+    return jsonify(person.to_dict())
 
-    return jsonify(response_body), 200
+# [GET] /planets - Get a list of all the planets
+@app.route('/planets', methods=['GET'])
+def get_planets():
+    planets = Planet.query.all()
+    return jsonify([planet.to_dict() for planet in planets])
+
+# [GET] /planets/<int:planet_id> - Get one single planet's information
+@app.route('/planets/<int:planet_id>', methods=['GET'])
+def get_planet(planet_id):
+    planet = Planet.query.get_or_404(planet_id)
+    return jsonify(planet.to_dict())
+
+# [GET] /users - Get a list of all the blog post users
+@app.route('/users', methods=['GET'])
+def get_users():
+    users = User.query.all()
+    return jsonify([user.to_dict() for user in users])
+
+# [GET] /users/favorites - Get all the favorites that belong to the current user
+@app.route('/users/favorites', methods=['GET'])
+def get_user_favorites():
+    # Assuming the "current user" is user with id=1 for simplicity
+    user = User.query.get(1)
+    return jsonify([favorite.to_dict() for favorite in user.favorites])
+
+# [POST] /favorite/planet/<int:planet_id> - Add a new favorite planet
+@app.route('/favorite/planet/<int:planet_id>', methods=['POST'])
+def add_favorite_planet(planet_id):
+    # Assuming the "current user" is user with id=1 for simplicity
+    favorite = Favorite(user_id=1, planet_id=planet_id)
+    db.session.add(favorite)
+    db.session.commit()
+    return jsonify(favorite.to_dict()), 201
+
+# [POST] /favorite/people/<int:people_id> - Add new favorite people
+@app.route('/favorite/people/<int:people_id>', methods=['POST'])
+def add_favorite_person(people_id):
+    favorite = Favorite(user_id=1, people_id=people_id)
+    db.session.add(favorite)
+    db.session.commit()
+    return jsonify(favorite.to_dict()), 201
+
+# [DELETE] /favorite/planet/<int:planet_id> - Delete a favorite planet
+@app.route('/favorite/planet/<int:planet_id>', methods=['DELETE'])
+def delete_favorite_planet(planet_id):
+    favorite = Favorite.query.filter_by(user_id=1, planet_id=planet_id).first_or_404()
+    db.session.delete(favorite)
+    db.session.commit()
+    return '', 204
+
+# [DELETE] /favorite/people/<int:people_id> - Delete a favorite person
+@app.route('/favorite/people/<int:people_id>', methods=['DELETE'])
+def delete_favorite_person(people_id):
+    favorite = Favorite.query.filter_by(user_id=1, people_id=people_id).first_or_404()
+    db.session.delete(favorite)
+    db.session.commit()
+    return '', 204
 
 # this only runs if `$ python src/app.py` is executed
 if __name__ == '__main__':
--- src/models.py
+++ src/models.py
@@ -3,17 +3,72 @@ from flask_sqlalchemy import SQLAlchemy
 db = SQLAlchemy()
 
 class User(db.Model):
+    __tablename__ = 'users'
     id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(80), unique=True, nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
-    password = db.Column(db.String(80), unique=False, nullable=False)
-    is_active = db.Column(db.Boolean(), unique=False, nullable=False)
 
-    def __repr__(self):
-        return '<User %r>' % self.username
+    favorites = db.relationship('Favorite', back_populates='user', lazy=True)
 
-    def serialize(self):
+    def to_dict(self):
         return {
             "id": self.id,
+            "username": self.username,
             "email": self.email,
-            # do not serialize the password, its a security breach
+            "favorites": [favorite.to_dict() for favorite in self.favorites]
+        }
+
+class People(db.Model):
+    __tablename__ = 'people'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(120), nullable=False)
+    birth_year = db.Column(db.String(10))
+    gender = db.Column(db.String(10))
+    eye_color = db.Column(db.String(10))
+    hair_color = db.Column(db.String(10))
+
+    def to_dict(self):
+        return {
+            "id": self.id,
+            "name": self.name,
+            "birth_year": self.birth_year,
+            "gender": self.gender,
+            "eye_color": self.eye_color,
+            "hair_color": self.hair_color
+        }
+
+class Planet(db.Model):
+    __tablename__ = 'planets'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(120), nullable=False)
+    climate = db.Column(db.String(120))
+    terrain = db.Column(db.String(120))
+    population = db.Column(db.Integer)
+
+    def to_dict(self):
+        return {
+            "id": self.id,
+            "name": self.name,
+            "climate": self.climate,
+            "terrain": self.terrain,
+            "population": self.population
+        }
+
+class Favorite(db.Model):
+    __tablename__ = 'favorites'
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
+    people_id = db.Column(db.Integer, db.ForeignKey('people.id'), nullable=True)
+    planet_id = db.Column(db.Integer, db.ForeignKey('planets.id'), nullable=True)
+
+    user = db.relationship('User', back_populates='favorites')
+    people = db.relationship('People', lazy='joined')
+    planet = db.relationship('Planet', lazy='joined')
+
+    def to_dict(self):
+        return {
+            "id": self.id,
+            "user_id": self.user_id,
+            "people": self.people.to_dict() if self.people else None,
+            "planet": self.planet.to_dict() if self.planet else None
         }
